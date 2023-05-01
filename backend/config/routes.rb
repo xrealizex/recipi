99462dc2a83475ce67f79adad4ee9278e5d08143
hiroto-hara <xre0605@gmail.com>
2023-05-01 21:53:02 +0900

    install devise gem

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
+  end
   get '/recipes/random', to: 'recipes#random'
   resources :recipes
   root to: 'recipes#index'
