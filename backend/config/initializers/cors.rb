f6893c7a31eb8b0d637d494d0d5394ba89c188be
hiroto-hara <xre0605@gmail.com>
2023-06-03 17:09:01 +0900

    env

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app"
+    origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app", 'https://recipe-choice-99mk1lzo2-hiroto-hara.vercel.app'
 
     resource "*",
       headers: :any,
