ffea1bbb3cd00b4bbca6c5d51a31658f201191e7
hiroto-hara <xre0605@gmail.com>
2023-06-03 22:00:44 +0900

    delete nginx

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -42,4 +42,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 app_root = File.expand_path('..', __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+# bind "unix://#{app_root}/tmp/sockets/puma.sock"
