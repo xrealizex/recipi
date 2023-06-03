beee4d861b52c4bdc44f89b79621540a97966522
hiroto-hara <xre0605@gmail.com>
2023-06-03 16:30:04 +0900

    setting cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://web.recipe-choice.shop"
+    origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app"
 
     resource "*",
       headers: :any,
