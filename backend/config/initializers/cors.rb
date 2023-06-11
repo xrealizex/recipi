f4bf9f787142cc4295048c905ca75a07dd24918e
hiroto-hara <xre0605@gmail.com>
2023-06-11 20:41:17 +0900

    fix cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,11 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app"
+    # origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app"
+    origins "*"
 
     resource "*",
       headers: :any,
-      expose: ["access-token", "expiry", "token-type", "uid", "client", "Access-Control-Allow-Origin"],
+      expose: ["access-token", "expiry", "token-type", "uid", "client"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -5,11 +5,6 @@ const options = {
   ignoreHeaders: true
 }
 
-// export const client = applyCaseMiddleware(axios.create({
-//   baseURL: process.env.REACT_APP_API_URL
-// }), options)
-
-
 export const client = applyCaseMiddleware(axios.create({
-  baseURL: "https://recipe-choice.onrender.com/api/v1"
+  baseURL: process.env.REACT_APP_API_URL
 }), options)
